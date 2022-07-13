0cb372f8960a10b9e391ef53f51ea77eff006834
brigitte.nikoll <roadclosed24@gmail.com>
2022-07-13 18:05:49 -0500

    Initial commit

---
+++ database.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+import certifi
+
+MONGO_URI ='mongodb+srv://brixs:ZisMPzZ6hmXjv7HZ@cluster0.tk5raxj.mongodb.net/?retryWrites=true&w=majority'
+ca = certifi.where()
+
+def dbConnection():
+    try:
+        client = MongoClient(MONGO_URI, tlsCAFile=ca)
+        db = client["dbb_products_app"]
+    except ConnectionError:
+        print('Error de conexión con la bdd')
+    return db
+
+    
